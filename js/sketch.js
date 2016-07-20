55cb126327e65f1ebca10801101925f8e0e7c2e5
yawkity <sehmon3@gmail.com>
2016-07-20 00:46:40 -0400

    Added experiments page and p5.js cdn

---
+++ js/sketch.js
@@ -0,0 +1,48 @@
+var rings = []
+
+function setup() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  createCanvas(width, height);
+  noFill();
+  background(200);
+
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 2, 2);
+  for(i=rings.length-1; i>= 0; i--){
+    if(rings[i].radius > window.innerWidth/2){
+      rings.splice(i, 1);
+    } else {
+      stroke(rings[i].start_color % 255)
+      rings[i].expand();
+      rings[i].display();
+    }
+  }
+}
+
+function mousePressed() {
+  r = new Ring();
+  r.x = mouseX;
+  r.y = mouseY;
+  rings.push(r);
+}
+
+function Ring() {
+  this.x;
+  this.y;
+  this.radius = 0;
+
+  this.start_color = 0;
+
+  this.expand = function(){
+    this.radius += 2;
+    this.start_color += 2;
+  };
+
+  this.display = function(){
+    ellipse(this.x, this.y, this.radius, this.radius);
+  };
+}
