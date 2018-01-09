1235254edba9495a1160c11b9ab55f2c79065532
sehmon <sehmon3@gmail.com>
2018-01-09 09:15:19 -0600

    Updated video sizes and added photos on mobile

--- js/home-sketch.js
+++ js/home-sketch.js
@@ -33,3 +33,23 @@ function draw() {
   // var xpos = Math.floor(mouseX / 40) * 40;
   // rect(xpos, 0, rectWidth, window.innerHeight);
 }
+
+// Replace videos with images on mobile
+if (isMobileDevice()){
+  var rogue = document.getElementById('rogue-media');
+  rogue.innerHTML = '<img src="img/rogue-demo.png"></img>'
+
+  var ao = document.getElementById('ao-media');
+  ao.innerHTML = '<img src="img/ao-demo.png"></img>'
+
+  var radio = document.getElementById('radio-media');
+  radio.innerHTML = '<img src="img/radio-demo.png"></img>'
+
+  var dialup = document.getElementById('dialup-media');
+  dialup.innerHTML = '<img src="img/dialup-demo.png"></img>'
+
+}
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
