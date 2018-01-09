61e7b788a380738968e8a3df993b3fddb65b4130
sehmon <sehmon3@gmail.com>
2018-01-09 09:43:58 -0600

    fixed image naming mistake

--- js/home-sketch.js
+++ js/home-sketch.js
@@ -37,16 +37,16 @@ function draw() {
 // Replace videos with images on mobile
 if (isMobileDevice()){
   var rogue = document.getElementById('rogue-media');
-  rogue.innerHTML = '<img src="img/rogue-demo.png" style="width: 75%"></img>'
+  rogue.innerHTML = '<img src="img/rogue-demo.png" style="width: 90%"></img>'
 
   var ao = document.getElementById('ao-media');
-  ao.innerHTML = '<img src="img/ao-demo.png" style="width: 75%"></img>'
+  ao.innerHTML = '<img src="img/ao-demo.png" style="width: 90%"></img>'
 
   var radio = document.getElementById('radio-media');
-  radio.innerHTML = '<img src="img/radio-demo.png" style="width: 75%"></img>'
+  radio.innerHTML = '<img src="img/radio-demo.png" style="width: 90%"></img>'
 
   var dialup = document.getElementById('dialup-media');
-  dialup.innerHTML = '<img src="img/dialup-demo.png" style="width: 75%"></img>'
+  dialup.innerHTML = '<img src="img/dialup-demo.png" style="width: 90%"></img>'
 
 }
 
